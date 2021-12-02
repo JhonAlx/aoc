85fbd1be4c3c30bac7784d75d494cfea5a0c0e77
Jhon Delgado <jhadelgadoca@unal.edu.co>
2021-12-02 07:08:14 -0500

    Create 2nd 1-2.cs

---
+++ 2nd 1-2.cs
@@ -0,0 +1,46 @@
+using System;
+using System.Collections.Generic;
+
+namespace aoc2021
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string line;
+            List<int> input = new List<int>();
+            int x = 0, y = 0;
+            
+            while(true)
+            {
+                line = Console.ReadLine();
+                
+                if(line == null)
+                    break;
+
+                string[] commands = line.Split(' ');
+                string direction = commands[0];
+                int amount = Int32.Parse(commands[1]);
+
+                switch(direction)
+                {
+                    case "forward":
+                        x += amount;
+                        break;
+                    
+                    case "down":
+                        y += amount;
+                        break;
+
+                    case "up":
+                        y -= amount;
+                        break;
+                }
+            }
+
+            
+
+            Console.WriteLine(x * y);
+        }
+    }
+}
