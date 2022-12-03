6ac79631bffa19e9afef03a1a3526d82fec2344b
Jhon <jhadelgadoca@unal.edu.co>
2022-12-03 06:46:35 -0500

    Create day3 - 1.cs

---
+++ 2022/day3 - 1.cs
@@ -0,0 +1,25 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace aoc
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            var counter = 0;
+            
+            foreach (string line in File.ReadAllLines("input.txt"))
+            {
+                var first = line.Substring(0, line.Length / 2).ToCharArray();
+                var second = line.Substring(line.Length / 2, line.Length / 2).ToCharArray();
+                var intersected = first.Intersect(second).Take(1).ToList()[0];
+
+                counter += char.IsUpper(intersected) ? (int)intersected - 38 : (int)intersected - 96;
+            }
+
+            Console.WriteLine(counter);
+        }
+    }
+}
